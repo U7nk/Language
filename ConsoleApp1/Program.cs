db02c7166a8931a6b00326a875ec96b13ea18029
Nechaev <noEmail@noDomain>
2022-10-20 16:31:01 +0500

    delete empty project, migrate to net7,

--- ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Threading.Channels;
-using Wired.CodeAnalysis;
-using Wired.CodeAnalysis.Syntax;
-
-var x = 55;
-Console.WriteLine(x);
