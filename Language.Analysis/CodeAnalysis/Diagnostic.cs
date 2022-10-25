ad17c2dac2a19c0a2b4000a569018c1a83dbe862
Nechaev <onlyforklim@gmail.com>
2022-10-25 23:36:28 +0500

    added string code for diagnostics

--- Language.Analysis/CodeAnalysis/Diagnostic.cs
+++ Language.Analysis/CodeAnalysis/Diagnostic.cs
@@ -4,13 +4,15 @@ namespace Language.Analysis.CodeAnalysis;
 
 public sealed class Diagnostic
 {
+    public string Code { get; }
     public TextLocation TextLocation { get; }
     public string Message { get; }
 
-    public Diagnostic(TextLocation textLocation, string message)
+    public Diagnostic(TextLocation textLocation, string message, string code)
     {
         TextLocation = textLocation;
         Message = message;
+        Code = code;
     }
 
     public override string ToString() => Message;
--- Language.Analysis/CodeAnalysis/DiagnosticBag.cs
+++ Language.Analysis/CodeAnalysis/DiagnosticBag.cs
@@ -9,242 +9,331 @@ namespace Language.Analysis.CodeAnalysis;
 
 public class DiagnosticBag : List<Diagnostic>
 {
-    public void Report(TextLocation textLocation, string message) 
-        => Add(new Diagnostic(textLocation, message));
+    public void Report(TextLocation textLocation, string message, string code)
+        => Add(new Diagnostic(textLocation, message, code));
 
+    public const string INVALID_NUMBER_CODE = "[0001:Error]";
 
     public void ReportInvalidNumber(TextLocation textLocation, string text, Type type)
     {
         var message = $"Number '{text}' is not a valid '{type}'.";
-        Report(textLocation, message);
+        Report(textLocation, message, INVALID_NUMBER_CODE);
     }
+
+    public const string BAD_CHARACTER_CODE = "[0002:Error]";
 
     public void ReportBadCharacter(TextLocation textLocation, char character)
     {
-        var message = $"error: bad character '{character}'.";
-        Report(textLocation, message);
+        var message = $"Bad character '{character}'.";
+        Report(textLocation, message, BAD_CHARACTER_CODE);
     }
+
+    public const string UNEXPECTED_TOKEN_CODE = "[0003:Error]";
 
     public void ReportUnexpectedToken(TextLocation textLocation, SyntaxKind token, SyntaxKind expected)
     {
-        var message = $"error: Unexpected token <{token}> expected <{expected}>.";
-        Report(textLocation, message);
+        var message = $"Unexpected token <{token}> expected <{expected}>.";
+        Report(textLocation, message, UNEXPECTED_TOKEN_CODE);
     }
+
+    public const string UNDEFINED_UNARY_OPERATOR_CODE = "[0004:Error]";
 
     public void ReportUndefinedUnaryOperator(TextLocation location, string text, TypeSymbol operandType)
     {
         var message = $"Invalid unary operator '{text}' for type '{operandType}'.";
-        Report(location, message);
+        Report(location, message, UNDEFINED_UNARY_OPERATOR_CODE);
     }
 
-    public void ReportUndefinedBinaryOperator(TextLocation location, string operatorText, TypeSymbol leftType, TypeSymbol rightType)
+    public const string UNDEFINED_BINARY_OPERATOR_CODE = "[0005:Error]";
+
+    public void ReportUndefinedBinaryOperator(TextLocation location, string operatorText, TypeSymbol leftType,
+        TypeSymbol rightType)
     {
         var message = $"Invalid binary operator '{operatorText}' for types '{leftType}' and '{rightType}'.";
-        Report(location, message);
+        Report(location, message, UNDEFINED_BINARY_OPERATOR_CODE);
     }
+
+    public const string UNDEFINED_NAME_CODE = "[0006:Error]";
 
     public void ReportUndefinedName(TextLocation location, string name)
     {
         var message = $"'{name}' is undefined.";
-        Report(location, message);
+        Report(location, message, UNDEFINED_NAME_CODE);
     }
 
+    public const string VARIABLE_ALREADY_DECLARED_CODE = "[0007:Error]";
+
     public void ReportVariableAlreadyDeclared(TextLocation location, string name)
-    { 
+    {
         var message = $"Variable '{name}' is already declared.";
-        Report(location, message);
+        Report(location, message, VARIABLE_ALREADY_DECLARED_CODE);
     }
+
+    public const string CANNOT_CONVERT_CODE = "[0008:Error]";
 
     public void ReportCannotConvert(TextLocation location, TypeSymbol fromType, TypeSymbol toType)
     {
         var message = $"Cannot convert '{fromType}' to '{toType}'.";
-        Report(location, message);
+        Report(location, message, CANNOT_CONVERT_CODE);
     }
+
+    public const string CANNOT_ASSIGN_TO_READONLY_CODE = "[0009:Error]";
 
     public void ReportCannotAssignToReadonly(TextLocation location, string name)
     {
         var message = $"'{name}' is readonly and cannot be assigned to.";
-        Report(location, message);
+        Report(location, message, CANNOT_ASSIGN_TO_READONLY_CODE);
     }
+
+    public const string VARIABLE_DOES_NOT_EXISTS_IN_CURRENT_SCOPE_CODE = "[0010:Error]";
 
     public void VariableDoesntExistsInCurrentScope(TextLocation location, string name)
     {
         var message = $"'{name}' doesn't exists in current scope.";
-        Report(location, message);
+        Report(location, message, VARIABLE_DOES_NOT_EXISTS_IN_CURRENT_SCOPE_CODE);
     }
+
+    public const string UNTERMINATED_STRING_CODE = "[0011:Error]";
 
     public void ReportUnterminatedString(TextLocation textLocation)
     {
         var message = "Unterminated string literal.";
-        Report(textLocation, message);
+        Report(textLocation, message, UNTERMINATED_STRING_CODE);
     }
+
+    public const string UNDEFINED_FUNCTION_CODE = "[0012:Error]";
 
     public void ReportUndefinedFunction(TextLocation location, string identifierText)
     {
         var message = $"Function '{identifierText}' is undefined.";
-        Report(location, message);
+        Report(location, message, UNDEFINED_FUNCTION_CODE);
     }
 
-    public void ReportParameterCountMismatch(TextLocation location, string identifierText, int parametersLength, int argumentsCount)
+    public const string PARAMETER_COUNT_MISMATCH_CODE = "[0013:Error]";
+
+    public void ReportParameterCountMismatch(TextLocation location, string identifierText, int parametersLength,
+        int argumentsCount)
     {
-        var message = $"Function '{identifierText}' requires {parametersLength} arguments but was given {argumentsCount}.";
-        Report(location, message);
+        var message =
+            $"Function '{identifierText}' requires {parametersLength} arguments but was given {argumentsCount}.";
+        Report(location, message, PARAMETER_COUNT_MISMATCH_CODE);
     }
+
+    public const string EXPRESSION_MUST_HAVE_VALUE_CODE = "[0014:Error]";
 
     public void ReportExpressionMustHaveValue(TextLocation location)
     {
         var message = $"Expression must have a value.";
-        Report(location, message);
+        Report(location, message, EXPRESSION_MUST_HAVE_VALUE_CODE);
     }
+
+    public const string UNDEFINED_TYPE_CODE = "[0015:Error]";
 
     public void ReportUndefinedType(TextLocation location, string typeName)
     {
         var message = $"Type '{typeName}' is undefined.";
-        Report(location, message);
+        Report(location, message, UNDEFINED_TYPE_CODE);
     }
+
+    public const string NO_IMPLICIT_CONVERSION_CODE = "[0016:Error]";
 
     public void ReportNoImplicitConversion(TextLocation location, TypeSymbol expressionType, TypeSymbol type)
     {
         var message = $"No implicit conversion from '{expressionType}' to '{type}'.";
-        Report(location, message);
+        Report(location, message, NO_IMPLICIT_CONVERSION_CODE);
     }
+
+    public const string TYPE_CLAUSE_EXPECTED_CODE = "[0017:Error]";
 
     public void ReportTypeClauseExpected(TextLocation location)
     {
         var message = $"Type clause expected.";
-        Report(location, message);
+        Report(location, message, TYPE_CLAUSE_EXPECTED_CODE);
     }
+
+    public const string PARAMETER_ALREADY_DECLARED_CODE = "[0018:Error]";
 
     public void ReportParameterAlreadyDeclared(TextLocation location, string name)
     {
         var message = $"Parameter '{name}' is already declared.";
-        Report(location, message);
+        Report(location, message, PARAMETER_ALREADY_DECLARED_CODE);
     }
+
+    public const string FUNCTION_ALREADY_DECLARED_CODE = "[0019:Error]";
 
     public void ReportFunctionAlreadyDeclared(TextLocation location, string identifierText)
     {
         var message = $"Function '{identifierText}' is already declared.";
-        Report(location, message);
+        Report(location, message, FUNCTION_ALREADY_DECLARED_CODE);
     }
+
+    public const string INVALID_BREAK_OR_CONTINUE_CODE = "[0020:Error]";
 
     public void ReportInvalidBreakOrContinue(SyntaxToken breakKeyword)
     {
         var message = $"Invalid use of '{breakKeyword.Text}'. Must be inside a loop.";
-        Report(breakKeyword.Location, message);
+        Report(breakKeyword.Location, message, INVALID_BREAK_OR_CONTINUE_CODE);
     }
+
+    public const string RETURN_STATEMENT_IS_INVALID_FOR_VOID_FUNCTION_CODE = "[0021:Error]";
 
     public void ReportReturnStatementIsInvalidForVoidFunction(TextLocation location)
     {
         var message = $"Return statement is invalid for {TypeSymbol.Void} function.";
-        Report(location, message);
+        Report(location, message, RETURN_STATEMENT_IS_INVALID_FOR_VOID_FUNCTION_CODE);
     }
+
+    public const string RETURN_STATEMENT_IS_INVALID_FOR_NON_VOID_FUNCTION_CODE = "[0022:Error]";
 
     public void ReportReturnStatementIsInvalidForNonVoidFunction(TextLocation location, TypeSymbol functionReturnType)
     {
         var message = $"return should have value for function with return type of {functionReturnType}.";
-        Report(location, message);
+        Report(location, message, RETURN_STATEMENT_IS_INVALID_FOR_NON_VOID_FUNCTION_CODE);
     }
+
+    public const string INVALID_RETURN_CODE = "[0023:Error]";
 
     public void ReportInvalidReturn(TextLocation location)
     {
         var message = $"Return statement should be inside a function.";
-        Report(location, message);
-        
+        Report(location, message, INVALID_RETURN_CODE);
     }
+
+    public const string ALL_PATHS_MUST_RETURN_CODE = "[0024:Error]";
 
     public void ReportAllPathsMustReturn(TextLocation location)
     {
         var message = $"All paths must return a value.";
-        Report(location, message);
+        Report(location, message, ALL_PATHS_MUST_RETURN_CODE);
     }
+
+    public const string INVALID_EXPRESSION_STATEMENT_CODE = "[0025:Error]";
 
     public void ReportInvalidExpressionStatement(TextLocation syntaxLocation)
     {
         var message = "Only assignment, and call expressions can be used as a statement.";
-        Report(syntaxLocation, message);
+        Report(syntaxLocation, message, INVALID_EXPRESSION_STATEMENT_CODE);
     }
+
+    public const string MAIN_CANNOT_BE_USED_WITH_GLOBAL_STATEMENTS_CODE = "[0026:Error]";
 
     public void ReportMainCannotBeUsedWithGlobalStatements(TextLocation identifierLocation)
     {
         var message = "Main cannot be used with global statements.";
-        Report(identifierLocation, message);
+        Report(identifierLocation, message, MAIN_CANNOT_BE_USED_WITH_GLOBAL_STATEMENTS_CODE);
     }
+
+    public const string MULTIPLE_MAIN_FUNCTIONS_CODE = "[0027:Error]";
 
     public void ReportMultipleMainFunctions(TextLocation identifierLocation)
     {
         var message = "Multiple main functions found.";
-        Report(identifierLocation, message);
+        Report(identifierLocation, message, MULTIPLE_MAIN_FUNCTIONS_CODE);
     }
+
+    public const string PARAMETER_SHOULD_HAVE_TYPE_EXPLICITLY_DEFINED_CODE = "[0028:Error]";
 
     public void ReportParameterShouldHaveTypeExplicitlyDefined(TextLocation parameterLocation, string parameterName)
     {
         var message = $"Parameter '{parameterName}' should have type explicitly defined.";
-        Report(parameterLocation, message);
+        Report(parameterLocation, message, PARAMETER_SHOULD_HAVE_TYPE_EXPLICITLY_DEFINED_CODE);
     }
+
+    public const string MAIN_MUST_HAVE_CORRECT_SIGNATURE_CODE = "[0029:Error]";
 
     public void ReportMainMustHaveCorrectSignature(TextLocation identifierLocation)
     {
         var message = $"main function must have correct signature(return type {TypeSymbol.Void} and 0 parameters).";
-        Report(identifierLocation, message);
+        Report(identifierLocation, message, MAIN_MUST_HAVE_CORRECT_SIGNATURE_CODE);
     }
+
+    public const string GLOBAL_STATEMENTS_SHOULD_ONLY_BE_IN_A_SINGLE_FILE_CODE = "[0030:Error]";
 
     public void ReportGlobalStatementsShouldOnlyBeInASingleFile(TextLocation location)
     {
         var message = $"Global statements should only be in a single file.";
-        Report(location, message);
+        Report(location, message, GLOBAL_STATEMENTS_SHOULD_ONLY_BE_IN_A_SINGLE_FILE_CODE);
     }
+
+    public const string CANNOT_EMIT_GLOBAL_STATEMENTS_BECAUSE_TYPE_ALREADY_EXISTS_CODE = "[0031:Error]";
 
     public void ReportCannotEmitGlobalStatementsBecauseTypeAlreadyExists(string typeName, TextLocation location)
     {
         var message = $"Cannot emit global statements because type '{typeName}' already exists.";
-        Report(location, message);
+        Report(location, message, CANNOT_EMIT_GLOBAL_STATEMENTS_BECAUSE_TYPE_ALREADY_EXISTS_CODE);
     }
+
+    public const string UNDEFINED_METHOD_CALL_CODE = "[0032:Error]";
 
     public void ReportUndefinedMethodCall(TextLocation rightLocation, string functionSymbolName, TypeSymbol leftType)
     {
         var message = $"Function '{functionSymbolName}' is undefined for type '{leftType}'.";
-        Report(rightLocation, message);
+        Report(rightLocation, message, UNDEFINED_METHOD_CALL_CODE);
     }
 
-    public void ReportAmbiguousType(TextLocation typeIdentifierLocation, string typeIdentifier, List<TypeSymbol> matchingTypes)
+    public const string AMBIGUOUS_TYPE_CODE = "[0033:Error]";
+
+    public void ReportAmbiguousType(TextLocation typeIdentifierLocation, string typeIdentifier,
+        List<TypeSymbol> matchingTypes)
     {
-        var message = $"Type '{typeIdentifier}' is ambiguous. Found {matchingTypes.Count} matching types: {string.Join(", ", matchingTypes.Select(t => t.Name))}.";
-        Report(typeIdentifierLocation, message);
+        var message =
+            $"Type '{typeIdentifier}' is ambiguous. Found {matchingTypes.Count} matching types: {string.Join(", ", matchingTypes.Select(t => t.Name))}.";
+        Report(typeIdentifierLocation, message, AMBIGUOUS_TYPE_CODE);
     }
+
+    public const string NO_MAIN_FUNCTION_ALLOWED_IN_SCRIPT_MODE_CODE = "[0034:Error]";
 
     public void ReportNoMainFunctionAllowedInScriptMode(TextLocation mainIdentifierLocation)
     {
         var message = $"No main function allowed in script mode. Use global statements instead.";
-        Report(mainIdentifierLocation, message);
+        Report(mainIdentifierLocation, message, NO_MAIN_FUNCTION_ALLOWED_IN_SCRIPT_MODE_CODE);
     }
+
+    public const string CLASS_WITH_THAT_NAME_IS_ALREADY_DECLARED_CODE = "[0035:Error]";
 
     public void ReportClassWithThatNameIsAlreadyDeclared(TextLocation classIdentifierLocation, string className)
     {
         var message = $"Class with name '{className}' is already declared.";
-        Report(classIdentifierLocation, message);
+        Report(classIdentifierLocation, message, CLASS_WITH_THAT_NAME_IS_ALREADY_DECLARED_CODE);
     }
+
+    public const string FIELD_ALREADY_DECLARED_CODE = "[0036:Error]";
 
     public void ReportFieldAlreadyDeclared(TextLocation fieldLocation, string fieldName)
     {
         var message = $"Field '{fieldName}' is already declared.";
-        Report(fieldLocation, message);
+        Report(fieldLocation, message, FIELD_ALREADY_DECLARED_CODE);
     }
+
+    public const string UNDEFINED_FIELD_ACCESS_CODE = "[0037:Error]";
 
     public void ReportUndefinedFieldAccess(SyntaxToken fieldIdentifier, TypeSymbol type)
     {
         var message = $"Field '{fieldIdentifier.Text}' is undefined for type '{type}'.";
-        Report(fieldIdentifier.Location, message);
+        Report(fieldIdentifier.Location, message, UNDEFINED_FIELD_ACCESS_CODE);
     }
+
+    public const string UNDEFINED_MEMBER_CODE = "[0038:Error]";
 
     public void ReportUndefinedMember(SyntaxToken memberIdentifierLocation, TypeSymbol type)
     {
         var message = $"Member '{memberIdentifierLocation.Text}' is undefined on type {type}.";
-        Report(memberIdentifierLocation.Location, message);
+        Report(memberIdentifierLocation.Location, message, UNDEFINED_MEMBER_CODE);
     }
+
+    public const string UNDEFINED_METHOD_CODE = "[0039:Error]";
 
     public void ReportUndefinedMethod(SyntaxToken methodCallIdentifier, TypeSymbol leftType)
     {
         var message = $"Method '{methodCallIdentifier.Text}' is undefined on type {leftType}.";
-        Report(methodCallIdentifier.Location, message);
+        Report(methodCallIdentifier.Location, message, UNDEFINED_METHOD_CODE);
+    }
+
+    public const string CLASS_MEMBER_CANNOT_HAVE_NAME_OF_CLASS_CODE = "[0040:Error]";
+
+    public void ReportClassMemberCannotHaveNameOfClass(SyntaxToken memberIdentifier)
+    {
+        var message = "Class member cannot have the same name as the class.";
+        Report(memberIdentifier.Location, message, CLASS_MEMBER_CANNOT_HAVE_NAME_OF_CLASS_CODE);
     }
 }
