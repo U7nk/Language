591c25cec34f0bece414d3132519004206865c0d
Nechaev <onlyforklim@gmail.com>
2022-10-25 23:35:59 +0500

    BoundScope: Merged all symbols in one dictionary, added ambiguity resolving

--- Language.Analysis/CodeAnalysis/Binding/BoundScope.cs
+++ Language.Analysis/CodeAnalysis/Binding/BoundScope.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Language.Analysis.CodeAnalysis.Symbols;
 
 namespace Language.Analysis.CodeAnalysis.Binding;
@@ -8,124 +10,210 @@ namespace Language.Analysis.CodeAnalysis.Binding;
 public class BoundScope
 {
     public BoundScope? Parent { get; }
-    readonly Dictionary<string, VariableSymbol> _variables = new();
-    readonly Dictionary<string, FunctionSymbol> _functions = new();
-    readonly Dictionary<string, TypeSymbol> _types = new();
-    readonly Dictionary<string, FieldSymbol> _fields = new();
+    
+    readonly Dictionary<string, List<Symbol>> _symbols = new();
 
     public BoundScope(BoundScope? parent)
     {
         Parent = parent;
     }
-
+    
     public bool TryDeclareVariable(VariableSymbol variable)
     {
-        if (_variables.ContainsKey(variable.Name))
-            return false;
-        
         if (Parent?.TryLookupVariable(variable.Name, out _) is true)
             return false;
         
-        _variables.Add(variable.Name, variable);
+        if (_symbols.TryGetValue(variable.Name, out var sameNamers))
+        {
+            var parameters = sameNamers.Where(x=> x.Kind == SymbolKind.Parameter);
+            if (parameters.Any())
+                return false;
+        
+            var variables = sameNamers.Where(x=> x.Kind == SymbolKind.Variable);
+            if (variables.Any())
+                return false;
+
+            sameNamers.Add(variable);
+        }
+        else
+        {
+            _symbols.Add(variable.Name, new List<Symbol> {variable});
+        }
         return true;
     }
     public bool TryLookupVariable(string name, [NotNullWhen(true)] out VariableSymbol? variable)
     {
-        if (_variables.TryGetValue(name, out variable))
-            return true;
+        variable = null;
+        if (_symbols.TryGetValue(name, out var sameNamers))
+        {
+            var variables = sameNamers.Where(x => x.Kind == SymbolKind.Variable).ToList();
+            Debug.Assert(variables.Count <= 1);
+            if (variables.Any())
+            {
+                variable = (VariableSymbol)variables.First();
+                return true;
+            }
+        }
 
+        // ReSharper disable once ConvertIfStatementToReturnStatement
         if (Parent != null)
             return Parent.TryLookupVariable(name, out variable);
-
-        variable = null;
+        
         return false;
     }
-    public ImmutableArray<VariableSymbol> GetDeclaredVariables()
-    {
-        return _variables.Values.ToImmutableArray();
-    }
-    
+    public ImmutableArray<VariableSymbol> GetDeclaredVariables() 
+        => _symbols.Values.SelectMany(x => x)
+            .OfType<VariableSymbol>()
+            .ToImmutableArray();
+
     public bool TryDeclareFunction(FunctionSymbol function)
     {
-        if (_functions.ContainsKey(function.Name))
-            return false;
-        
         if (Parent?.TryLookupFunction(function.Name, out _) is true)
             return false;
         
-        _functions.Add(function.Name, function);
+        if (_symbols.TryGetValue(function.Name, out var sameNamers))
+        {
+            var functionsAndFieldsAndTypes = sameNamers.Where(
+                x => x.Kind 
+                    is SymbolKind.Function 
+                    or SymbolKind.Field 
+                    or SymbolKind.Type);
+            
+            if (functionsAndFieldsAndTypes.Any())
+                return false;
+            
+            sameNamers.Add(function);
+        }
+        else
+        {
+            _symbols.Add(function.Name,new List<Symbol>{ function });    
+        }
         return true;
     }
     public bool TryLookupFunction(string name, [NotNullWhen(true)] out FunctionSymbol? function)
     {
-        if (_functions.TryGetValue(name, out var result))
+        function = null;
+        
+        if (_symbols.TryGetValue(name, out var sameNamers))
         {
-            result.Unwrap();
-            function = result;
-            return true;
+            var functions = sameNamers.Where(x => x.Kind == SymbolKind.Function).ToList();
+            Debug.Assert(functions.Count <= 1);
+            if (functions.Any())
+            {
+                function = (FunctionSymbol)functions.First();
+                return true;
+            }
         }
 
+        // ReSharper disable once ConvertIfStatementToReturnStatement
         if (Parent != null)
             return Parent.TryLookupFunction(name, out function);
-
-        function = null;
+        
         return false;
     }
-    public ImmutableArray<FunctionSymbol> GetDeclaredFunctions()
-    {
-        return _functions.Values.ToImmutableArray();
-    }
-    
+    public ImmutableArray<FunctionSymbol> GetDeclaredFunctions() 
+        => _symbols.Values.SelectMany(x => x)
+            .OfType<FunctionSymbol>()
+            .ToImmutableArray();
+
     public bool TryDeclareType(TypeSymbol typeSymbol)
     {
-        if (_types.ContainsKey(typeSymbol.Name))
-            return false;
-        
         if (Parent?.TryLookupType(typeSymbol.Name, out _) is true)
             return false;
         
-        _types.Add(typeSymbol.Name, typeSymbol);
+        if (_symbols.TryGetValue(typeSymbol.Name, out var sameNamers))
+        {
+            var functionsAndFieldsAndTypes = sameNamers.Where(
+                x => x.Kind 
+                    is SymbolKind.Function 
+                    or SymbolKind.Field 
+                    or SymbolKind.Type);
+            
+            if (functionsAndFieldsAndTypes.Any())
+                return false;
+            
+            sameNamers.Add(typeSymbol);
+        }
+        else
+        {
+            _symbols.Add(typeSymbol.Name,new List<Symbol>{ typeSymbol });    
+        }
+        
         return true;
     }
+    
     public bool TryLookupType(string name, [NotNullWhen(true)] out TypeSymbol? type)
     {
-        if (_types.TryGetValue(name, out type))
-            return true;
-        
+        type = null;
+        if (_symbols.TryGetValue(name, out var sameNamers))
+        {
+            var types = sameNamers.Where(x => x.Kind == SymbolKind.Type).ToList();
+            Debug.Assert(types.Count <= 1);
+            if (types.Any())
+            {
+                type = (TypeSymbol)types.First();
+                return true;
+            }
+        }
+
         if (Parent?.TryLookupType(name, out type) is true)
             return Parent.TryLookupType(name, out type);
         
         return false;
     }
-    public ImmutableArray<TypeSymbol> GetDeclaredTypes()
-    {
-        return _types.Values.ToImmutableArray();
-    }
-    
+    public ImmutableArray<TypeSymbol> GetDeclaredTypes() 
+        => _symbols.Values.SelectMany(x=> x)
+            .OfType<TypeSymbol>()
+            .ToImmutableArray();
+
     public bool TryDeclareField(FieldSymbol fieldSymbol)
     {
-        if (_fields.ContainsKey(fieldSymbol.Name))
-            return false;
-        
         if (Parent?.TryLookupField(fieldSymbol.Name, out _) is true)
             return false;
         
-        _fields.Add(fieldSymbol.Name, fieldSymbol);
+        if (_symbols.TryGetValue(fieldSymbol.Name, out var sameNamers))
+        {
+            var functionsAndFieldsAndTypes = sameNamers.Where(
+                x => x.Kind 
+                    is SymbolKind.Function 
+                    or SymbolKind.Field 
+                    or SymbolKind.Type);
+            
+            if (functionsAndFieldsAndTypes.Any())
+                return false;
+            
+            sameNamers.Add(fieldSymbol);
+        }
+        else
+        {
+            _symbols.Add(fieldSymbol.Name,new List<Symbol>{ fieldSymbol });    
+        }
+        
         return true;
     }
+    
     public bool TryLookupField(string name, [NotNullWhen(true)] out FieldSymbol? field)
     {
-        if (_fields.TryGetValue(name, out field))
-            return true;
-        
+        field = null;
+        if (_symbols.TryGetValue(name, out var sameNamers))
+        {
+            var fields = sameNamers.Where(x => x.Kind == SymbolKind.Field).ToList();
+            Debug.Assert(fields.Count <= 1);
+            if (fields.Any())
+            {
+                field = (FieldSymbol)fields.First();
+                return true;
+            }
+        }
+
+        // ReSharper disable once ConvertIfStatementToReturnStatement
         if (Parent?.TryLookupField(name, out field) is true)
             return Parent.TryLookupField(name, out field);
         
         return false;
     }
-    public ImmutableArray<FieldSymbol> GetDeclaredFields()
-    {
-        return _fields.Values.ToImmutableArray();
-    }
-    
+    public ImmutableArray<FieldSymbol> GetDeclaredFields() 
+        => _symbols.Values.SelectMany(x => x)
+            .OfType<FieldSymbol>()
+            .ToImmutableArray();
 }
